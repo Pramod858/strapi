5afbc905a6ed187be2f40c5f0adaf3ae7ca58803
Pramod Badiger <80105491+Pramod858@users.noreply.github.com>
2024-06-30 21:02:17 +0530

    Update Strapi.sh

--- Terraform/Strapi.sh
+++ Terraform/Strapi.sh
@@ -2,7 +2,7 @@
 
 # Update and install necessary packages
 sudo apt-get update
-sudo apt-get install -y docker.io git
+sudo apt-get install -y docker.io
 
 # Clone your Git repository (adjust the URL accordingly)
 git clone -b devops https://github.com/Pramod858/simple-strapi.git
@@ -10,8 +10,11 @@ git clone -b devops https://github.com/Pramod858/simple-strapi.git
 # Navigate to the directory with Dockerfile
 cd /home/ubuntu/simple-strapi
 
+#  Change docker permissions
+sudo chmod 666 /var/run/docker.sock
+
 # Build the Docker image
-sudo docker build -t strapi .
+docker build -t strapi .
 
 # Run the Docker container (you may need to adjust port mapping and other options)
-sudo docker run -d -p 1337:1337 strapi
+docker run -d -p 1337:1337 strapi
