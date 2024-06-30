b5a0a4da683c09be7b8591805a937a1264fc9c4c
Pramod Badiger <80105491+Pramod858@users.noreply.github.com>
2024-06-30 20:53:28 +0530

    Update Strapi.sh

--- Terraform/Strapi.sh
+++ Terraform/Strapi.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
+
+# Update and install necessary packages
 sudo apt-get update
-# Remove the conflicting packages
-sudo apt-get remove -y libnode-dev libnode72
-# Install Node.js and npm
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo npm install -g pm2
-# Create the /srv/strapi directory
-sudo mkdir -p /srv/strapi
-sudo chown -R ubuntu:ubuntu /srv/strapi
-# Clone the repository
-git clone https://github.com/Pramod858/simple-strapi /srv/strapi
-cd /srv/strapi
-chmod +x generate_env_var.sh
-./generate_env_var.sh
-npm install
-npm run build
-# Start the Strapi application using pm2
-pm2 start npm --name "strapi" -- start
+sudo apt-get install -y docker.io git
+
+# Clone your Git repository (adjust the URL accordingly)
+git clone -b devops https://github.com/Pramod858/simple-strapi.git
+
+# Navigate to the directory with Dockerfile
+cd /home/ubuntu/simple-strapi
+
+# Build the Docker image
+sudo docker build -t strapi .
+
+# Run the Docker container (you may need to adjust port mapping and other options)
+sudo docker run -d -p 1337:1337 strapi
